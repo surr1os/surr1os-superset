e592e3bc22fe009b5cf38759d9a2917e1bb4a2be
Konstantin <kostya.kazakov.300001@mail.ru>
2026-06-08 21:55:41 +0500

    [init]

---
+++ superset/tasks/cache.py
@@ -0,0 +1,271 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+from __future__ import annotations
+
+import logging
+from typing import Any, Optional, Union
+
+from celery.utils.log import get_task_logger
+from flask import current_app
+from sqlalchemy import and_, func
+from sqlalchemy.orm import selectinload
+
+from superset import db, security_manager
+from superset.extensions import celery_app
+from superset.models.core import Log
+from superset.models.dashboard import Dashboard
+from superset.tags.models import Tag, TaggedObject
+from superset.utils.date_parser import parse_human_datetime
+from superset.utils.webdriver import WebDriverSelenium
+
+logger = get_task_logger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def get_dash_url(dashboard: Dashboard) -> str:
+    """Return external URL for warming up a given dashboard cache."""
+    with current_app.test_request_context():
+        baseurl = (
+            # when running this as an async task, drop the request context with
+            # app.test_request_context()
+            current_app.config.get("WEBDRIVER_BASEURL")
+            or "{SUPERSET_WEBSERVER_PROTOCOL}://"
+            "{SUPERSET_WEBSERVER_ADDRESS}:"
+            "{SUPERSET_WEBSERVER_PORT}".format(**current_app.config)
+        )
+        return f"{baseurl.rstrip('/')}{dashboard.url}"
+
+
+class Strategy:  # pylint: disable=too-few-public-methods
+    """
+    A cache warm up strategy.
+
+    Each strategy defines a `get_urls` method that returns a list of dashboard URLs to
+    warm up using WebDriver.
+
+    Strategies can be configured in `superset/config.py`:
+
+        beat_schedule = {
+            'cache-warmup-hourly': {
+                'task': 'cache-warmup',
+                'schedule': crontab(minute=1, hour='*'),  # @hourly
+                'kwargs': {
+                    'strategy_name': 'top_n_dashboards',
+                    'top_n': 10,
+                    'since': '7 days ago',
+                },
+            },
+        }
+
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def get_urls(self) -> list[str]:
+        raise NotImplementedError("Subclasses must implement get_urls!")
+
+
+class DummyStrategy(Strategy):  # pylint: disable=too-few-public-methods
+    """
+    Warm up all published dashboards.
+
+    This is a dummy strategy that will fetch all published dashboards.
+    Can be configured by:
+
+        beat_schedule = {
+            'cache-warmup-hourly': {
+                'task': 'cache-warmup',
+                'schedule': crontab(minute=1, hour='*'),  # @hourly
+                'kwargs': {'strategy_name': 'dummy'},
+            },
+        }
+
+    """
+
+    name = "dummy"
+
+    def get_urls(self) -> list[str]:
+        # Use selectinload to avoid N+1 queries when checking dashboard.slices
+        dashboards = (
+            db.session.query(Dashboard)
+            .options(selectinload(Dashboard.slices))
+            .filter(Dashboard.published.is_(True))
+            .all()
+        )
+
+        return [get_dash_url(dashboard) for dashboard in dashboards if dashboard.slices]
+
+
+class TopNDashboardsStrategy(Strategy):  # pylint: disable=too-few-public-methods
+    """
+    Warm up charts in the top-n dashboards.
+
+        beat_schedule = {
+            'cache-warmup-hourly': {
+                'task': 'cache-warmup',
+                'schedule': crontab(minute=1, hour='*'),  # @hourly
+                'kwargs': {
+                    'strategy_name': 'top_n_dashboards',
+                    'top_n': 5,
+                    'since': '7 days ago',
+                },
+            },
+        }
+
+    """
+
+    name = "top_n_dashboards"
+
+    def __init__(self, top_n: int = 5, since: str = "7 days ago") -> None:
+        super().__init__()
+        self.top_n = top_n
+        self.since = parse_human_datetime(since) if since else None
+
+    def get_urls(self) -> list[str]:
+        records = (
+            db.session.query(Log.dashboard_id, func.count(Log.dashboard_id))
+            .filter(and_(Log.dashboard_id.isnot(None), Log.dttm >= self.since))
+            .group_by(Log.dashboard_id)
+            .order_by(func.count(Log.dashboard_id).desc())
+            .limit(self.top_n)
+            .all()
+        )
+        dash_ids = [record.dashboard_id for record in records]
+        dashboards = (
+            db.session.query(Dashboard).filter(Dashboard.id.in_(dash_ids)).all()
+        )
+
+        return [get_dash_url(dashboard) for dashboard in dashboards]
+
+
+class DashboardTagsStrategy(Strategy):  # pylint: disable=too-few-public-methods
+    """
+    Warm up charts in dashboards with custom tags.
+
+        beat_schedule = {
+            'cache-warmup-hourly': {
+                'task': 'cache-warmup',
+                'schedule': crontab(minute=1, hour='*'),  # @hourly
+                'kwargs': {
+                    'strategy_name': 'dashboard_tags',
+                    'tags': ['core', 'warmup'],
+                },
+            },
+        }
+    """
+
+    name = "dashboard_tags"
+
+    def __init__(self, tags: Optional[list[str]] = None) -> None:
+        super().__init__()
+        self.tags = tags or []
+
+    def get_urls(self) -> list[str]:
+        urls = []
+        tags = db.session.query(Tag).filter(Tag.name.in_(self.tags)).all()
+        tag_ids = [tag.id for tag in tags]
+
+        # add dashboards that are tagged
+        tagged_objects = (
+            db.session.query(TaggedObject)
+            .filter(
+                and_(
+                    TaggedObject.object_type == "dashboard",
+                    TaggedObject.tag_id.in_(tag_ids),
+                )
+            )
+            .all()
+        )
+        dash_ids = [tagged_object.object_id for tagged_object in tagged_objects]
+        tagged_dashboards = db.session.query(Dashboard).filter(
+            Dashboard.id.in_(dash_ids)
+        )
+        for dashboard in tagged_dashboards:
+            urls.append(get_dash_url(dashboard))
+
+        return urls
+
+
+strategies = [DummyStrategy, TopNDashboardsStrategy, DashboardTagsStrategy]
+
+
+@celery_app.task(name="cache-warmup")
+def cache_warmup(
+    strategy_name: str, *args: Any, **kwargs: Any
+) -> Union[dict[str, list[str]], str]:
+    """
+    Warm up cache.
+
+    This task periodically hits dashboards to warm up the cache.
+
+    """
+    logger.info("Loading strategy")
+    class_ = None
+    for class_ in strategies:
+        if class_.name == strategy_name:  # type: ignore
+            break
+    else:
+        message = "No strategy %s found!" % strategy_name
+        logger.error(message, exc_info=True)
+        return message
+
+    logger.info("Loading %s", class_.__name__)
+    try:
+        strategy = class_(*args, **kwargs)
+        logger.info("Success!")
+    except TypeError:
+        message = "Error loading strategy!"
+        logger.exception(message)
+        return message
+
+    results: dict[str, list[str]] = {"success": [], "errors": []}
+
+    warmup_username = current_app.config.get("SUPERSET_CACHE_WARMUP_USER")
+    if not warmup_username:
+        message = (
+            "SUPERSET_CACHE_WARMUP_USER is not configured. Set it to a dedicated "
+            "least-privilege user with access to the dashboards you want warmed up."
+        )
+        logger.error(message)
+        return message
+
+    user = security_manager.find_user(username=warmup_username)
+    if not user:
+        message = (
+            f"Cache warmup user '{warmup_username}' not found. Please configure "
+            "SUPERSET_CACHE_WARMUP_USER with a valid username."
+        )
+        logger.error(message)
+        return message
+
+    wd = WebDriverSelenium(current_app.config["WEBDRIVER_TYPE"], user=user)
+
+    try:
+        for url in strategy.get_urls():
+            try:
+                logger.info("Fetching %s", url)
+                wd.get_screenshot(url, "grid-container")
+                results["success"].append(url)
+            except Exception:  # noqa: BLE001
+                logger.exception("Error warming up cache for %s", url)
+                results["errors"].append(url)
+    finally:
+        # Ensure WebDriver is properly cleaned up
+        wd.destroy()
+
+    return results
---
+++ tests/unit_tests/result_set_test.py
@@ -0,0 +1,387 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+# pylint: disable=import-outside-toplevel, unused-argument
+
+from datetime import datetime, timezone
+
+import numpy as np
+import pandas as pd
+from numpy.core.multiarray import array
+from pytest_mock import MockerFixture
+
+from superset.db_engine_specs.base import BaseEngineSpec
+from superset.result_set import stringify_values, SupersetResultSet
+from superset.superset_typing import DbapiResult
+from superset.utils import json as superset_json
+
+
+def test_column_names_as_bytes() -> None:
+    """
+    Test that we can handle column names as bytes.
+    """
+    from superset.db_engine_specs.redshift import RedshiftEngineSpec
+    from superset.result_set import SupersetResultSet
+
+    data = (
+        [
+            "2016-01-26",
+            392.002014,
+            397.765991,
+            390.575012,
+            392.153015,
+            392.153015,
+            58147000,
+        ],
+        [
+            "2016-01-27",
+            392.444,
+            396.842987,
+            391.782013,
+            394.971985,
+            394.971985,
+            47424400,
+        ],
+    )
+    description = [
+        (b"date", 1043, None, None, None, None, None),
+        (b"open", 701, None, None, None, None, None),
+        (b"high", 701, None, None, None, None, None),
+        (b"low", 701, None, None, None, None, None),
+        (b"close", 701, None, None, None, None, None),
+        (b"adj close", 701, None, None, None, None, None),
+        (b"volume", 20, None, None, None, None, None),
+    ]
+    result_set = SupersetResultSet(data, description, RedshiftEngineSpec)  # type: ignore
+
+    assert (
+        result_set.to_pandas_df().to_markdown()
+        == """
+|    | date       |    open |    high |     low |   close |   adj close |   volume |
+|---:|:-----------|--------:|--------:|--------:|--------:|------------:|---------:|
+|  0 | 2016-01-26 | 392.002 | 397.766 | 390.575 | 392.153 |     392.153 | 58147000 |
+|  1 | 2016-01-27 | 392.444 | 396.843 | 391.782 | 394.972 |     394.972 | 47424400 |
+    """.strip()
+    )
+
+
+def test_stringify_with_null_integers():
+    """
+    Test that we can safely handle type errors when an integer column has a null value
+    """
+
+    data = [
+        ("foo", "bar", pd.NA, None),
+        ("foo", "bar", pd.NA, True),
+        ("foo", "bar", pd.NA, None),
+    ]
+    numpy_dtype = [
+        ("id", "object"),
+        ("value", "object"),
+        ("num", "object"),
+        ("bool", "object"),
+    ]
+
+    array2 = np.array(data, dtype=numpy_dtype)
+    column_names = ["id", "value", "num", "bool"]
+
+    result_set = np.array([stringify_values(array2[column]) for column in column_names])
+
+    expected = np.array(
+        [
+            array(["foo", "foo", "foo"], dtype=object),
+            array(["bar", "bar", "bar"], dtype=object),
+            array([None, None, None], dtype=object),
+            array([None, "True", None], dtype=object),
+        ]
+    )
+
+    assert np.array_equal(result_set, expected)
+
+
+def test_stringify_with_null_timestamps():
+    """
+    Test that we can safely handle type errors when a timestamp column has a null value
+    """
+
+    data = [
+        ("foo", "bar", pd.NaT, None),
+        ("foo", "bar", pd.NaT, True),
+        ("foo", "bar", pd.NaT, None),
+    ]
+    numpy_dtype = [
+        ("id", "object"),
+        ("value", "object"),
+        ("num", "object"),
+        ("bool", "object"),
+    ]
+
+    array2 = np.array(data, dtype=numpy_dtype)
+    column_names = ["id", "value", "num", "bool"]
+
+    result_set = np.array([stringify_values(array2[column]) for column in column_names])
+
+    expected = np.array(
+        [
+            array(["foo", "foo", "foo"], dtype=object),
+            array(["bar", "bar", "bar"], dtype=object),
+            array([None, None, None], dtype=object),
+            array([None, "True", None], dtype=object),
+        ]
+    )
+
+    assert np.array_equal(result_set, expected)
+
+
+def test_timezone_series(mocker: MockerFixture) -> None:
+    """
+    Test that we can handle timezone-aware datetimes correctly.
+
+    This covers a regression that happened when upgrading from Pandas 1.5.3 to 2.0.3.
+    """
+    logger = mocker.patch("superset.result_set.logger")
+
+    data = [[datetime(2023, 1, 1, tzinfo=timezone.utc)]]
+    description = [(b"__time", "datetime", None, None, None, None, False)]
+    result_set = SupersetResultSet(
+        data,
+        description,  # type: ignore
+        BaseEngineSpec,
+    )
+    assert result_set.to_pandas_df().values.tolist() == [
+        [pd.Timestamp("2023-01-01 00:00:00+0000", tz="UTC")]
+    ]
+    logger.exception.assert_not_called()
+
+
+def test_out_of_bounds_datetime_coerced_to_nat(mocker: MockerFixture) -> None:
+    """
+    Dates beyond ~2262-04-11 overflow pandas' int64 nanosecond representation.
+    SupersetResultSet must coerce them to NaT rather than raising OutOfBoundsDatetime
+    and logging an ERROR (which would surface as noise in observability tooling).
+    """
+    logger = mocker.patch("superset.result_set.logger")
+
+    data = [[datetime(3118, 1, 1, tzinfo=timezone.utc)]]
+    description = [(b"dt", "datetime", None, None, None, None, False)]
+    result_set = SupersetResultSet(
+        data,
+        description,  # type: ignore
+        BaseEngineSpec,
+    )
+    df = result_set.to_pandas_df()
+    assert pd.isna(df["dt"].iloc[0])
+    logger.exception.assert_not_called()
+
+
+def test_get_column_description_from_empty_data_using_cursor_description(
+    mocker: MockerFixture,
+) -> None:
+    """
+    Test that we can handle get_column_decription from the cursor description
+    when data is empty
+    """
+    logger = mocker.patch("superset.result_set.logger")
+
+    data: DbapiResult = []
+    description = [(b"__time", "datetime", None, None, None, None, 1, 0, 255)]
+    result_set = SupersetResultSet(
+        data,
+        description,  # type: ignore
+        BaseEngineSpec,
+    )
+    assert any(col.get("column_name") == "__time" for col in result_set.columns)
+    logger.exception.assert_not_called()
+
+
+def test_empty_column_names_get_synthetic_names() -> None:
+    """
+    SQL Server returns an empty-string column name in cursor.description for
+    any un-aliased expression (e.g. ``SELECT COUNT(*) FROM t``).  An empty
+    field name is illegal in NumPy structured arrays and PyArrow tables.
+
+    SupersetResultSet must replace empty column names with synthetic names
+    so queries like ``SELECT COUNT(*) FROM t`` succeed on MSSQL.
+
+    Regression test for https://github.com/apache/superset/issues/23848
+    """
+    data = [(42,)]
+    description = [("", 3, None, None, None, None, None)]
+    result_set = SupersetResultSet(data, description, BaseEngineSpec)  # type: ignore
+
+    assert result_set.columns[0]["column_name"] == "_col_0"
+    df = result_set.to_pandas_df()
+    assert list(df.columns) == ["_col_0"]
+    assert df["_col_0"].iloc[0] == 42
+
+
+def test_multiple_empty_column_names_get_unique_synthetic_names() -> None:
+    """
+    When several columns have empty names (e.g. ``SELECT COUNT(*), SUM(x)``
+    on MSSQL), each must receive a distinct synthetic name.
+    """
+    data = [(10, 20)]
+    description = [
+        ("", 3, None, None, None, None, None),
+        ("", 3, None, None, None, None, None),
+    ]
+    result_set = SupersetResultSet(data, description, BaseEngineSpec)  # type: ignore
+
+    col_names = [c["column_name"] for c in result_set.columns]
+    assert len(col_names) == 2
+    assert len(set(col_names)) == 2  # all unique
+    df = result_set.to_pandas_df()
+    assert df.iloc[0].tolist() == [10, 20]
+
+
+def test_empty_column_names_do_not_rename_explicit_synthetic_names() -> None:
+    """
+    Synthetic names assigned to empty columns must not collide with explicit
+    user-selected names that already look like Superset fallbacks.
+    """
+    data = [(10, 20)]
+    description = [
+        ("", 3, None, None, None, None, None),
+        ("_col_0", 3, None, None, None, None, None),
+    ]
+    result_set = SupersetResultSet(data, description, BaseEngineSpec)  # type: ignore
+
+    col_names = [c["column_name"] for c in result_set.columns]
+    assert col_names == ["_col_1", "_col_0"]
+    df = result_set.to_pandas_df()
+    assert list(df.columns) == ["_col_1", "_col_0"]
+    assert df.iloc[0].tolist() == [10, 20]
+
+
+def test_json_data_type_preserved_as_objects() -> None:
+    """
+    Test that JSON/JSONB data is preserved as Python objects (dicts/lists)
+    instead of being converted to strings.
+
+    This is important for Handlebars templates and other features that need
+    to access JSON data as objects rather than strings.
+
+    See: https://github.com/apache/superset/issues/25125
+    """
+    # Simulate data from PostgreSQL JSONB column - psycopg2 returns dicts
+    data = [
+        (1, {"key": "value1", "nested": {"a": 1}}, "text1"),
+        (2, {"key": "value2", "items": [1, 2, 3]}, "text2"),
+        (3, None, "text3"),
+        (4, {"mixed": "string"}, "text4"),
+    ]
+    description = [
+        ("id", 23, None, None, None, None, None),  # INT
+        ("json_col", 3802, None, None, None, None, None),  # JSONB
+        ("text_col", 1043, None, None, None, None, None),  # VARCHAR
+    ]
+    result_set = SupersetResultSet(data, description, BaseEngineSpec)  # type: ignore
+    df = result_set.to_pandas_df()
+
+    # JSON column should be preserved as Python objects, not strings
+    assert df["json_col"].iloc[0] == {"key": "value1", "nested": {"a": 1}}
+    assert isinstance(df["json_col"].iloc[0], dict)
+    assert df["json_col"].iloc[1] == {"key": "value2", "items": [1, 2, 3]}
+    assert df["json_col"].iloc[2] is None
+    assert df["json_col"].iloc[3] == {"mixed": "string"}
+
+    # Plain TEXT/VARCHAR columns must be left untouched as strings, even when
+    # adjacent to a JSON column.
+    assert df["text_col"].iloc[0] == "text1"
+    assert df["text_col"].iloc[3] == "text4"
+
+    # Verify the data can be serialized to JSON (as it would be for API response)
+    records = df.to_dict(orient="records")
+    json_output = superset_json.dumps(records)
+    parsed = superset_json.loads(json_output)
+    assert parsed[0]["json_col"]["key"] == "value1"
+    assert parsed[0]["json_col"]["nested"]["a"] == 1
+    assert parsed[1]["json_col"]["items"] == [1, 2, 3]
+
+
+def test_json_formatted_string_in_text_column_stays_string() -> None:
+    """
+    A plain TEXT/VARCHAR column whose values happen to be JSON-formatted strings
+    must be left unchanged as strings. There is no content-sniffing: only columns
+    that the driver returns as actual nested Python objects (dicts/lists) are
+    preserved as objects.
+
+    See: https://github.com/apache/superset/issues/25125
+    """
+    data = [
+        (1, '{"key": "val"}'),
+        (2, "[1, 2, 3]"),
+        (3, "not json at all"),
+    ]
+    description = [
+        ("id", 23, None, None, None, None, None),  # INT
+        ("text_col", 1043, None, None, None, None, None),  # VARCHAR
+    ]
+    result_set = SupersetResultSet(data, description, BaseEngineSpec)  # type: ignore
+    df = result_set.to_pandas_df()
+
+    # Values stay as raw strings, never parsed into dict/list.
+    assert df["text_col"].iloc[0] == '{"key": "val"}'
+    assert isinstance(df["text_col"].iloc[0], str)
+    assert df["text_col"].iloc[1] == "[1, 2, 3]"
+    assert isinstance(df["text_col"].iloc[1], str)
+    assert df["text_col"].iloc[2] == "not json at all"
+
+
+def test_json_data_with_homogeneous_structure() -> None:
+    """
+    Test that JSON data with consistent structure is also preserved as objects.
+    """
+    # All rows have the same JSON structure
+    data = [
+        (1, {"name": "Alice", "age": 30}),
+        (2, {"name": "Bob", "age": 25}),
+        (3, {"name": "Charlie", "age": 35}),
+    ]
+    description = [
+        ("id", 23, None, None, None, None, None),
+        ("data", 3802, None, None, None, None, None),
+    ]
+    result_set = SupersetResultSet(data, description, BaseEngineSpec)  # type: ignore
+    df = result_set.to_pandas_df()
+
+    # Should be preserved as dicts
+    assert isinstance(df["data"].iloc[0], dict)
+    assert df["data"].iloc[0]["name"] == "Alice"
+    assert df["data"].iloc[1]["age"] == 25
+
+
+def test_array_data_type_preserved() -> None:
+    """
+    Test that array data is also preserved as Python lists.
+    """
+    data = [
+        (1, [1, 2, 3]),
+        (2, [4, 5, 6]),
+        (3, None),
+    ]
+    description = [
+        ("id", 23, None, None, None, None, None),
+        ("arr", 1007, None, None, None, None, None),  # INT ARRAY
+    ]
+    result_set = SupersetResultSet(data, description, BaseEngineSpec)  # type: ignore
+    df = result_set.to_pandas_df()
+
+    # Arrays should be preserved as lists
+    assert df["arr"].iloc[0] == [1, 2, 3]
+    assert isinstance(df["arr"].iloc[0], list)
+    assert df["arr"].iloc[2] is None
